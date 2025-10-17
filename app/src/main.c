6cba57b7ca990c071ff96dd7f59c56b673843b00
Carl Elgario <kirstencarl.elgario@ucalgary.ca>
2025-10-16 20:28:26 -0600

    Begin 4-bit counter exercise

--- app/src/main.c
+++ app/src/main.c
@@ -3,48 +3,44 @@
  */
 
 #include <zephyr/kernel.h>
-#include <zephyr/device.h>
-#include <zephyr/drivers/gpio.h>
-#include <zephyr/sys/printk.h>
-#include <inttypes.h>
+#include <BTN.h>
+#include <LED.h>
 
-#define SLEEP_TIME_MS   1000
-#define SW0_NODE        DT_ALIAS(sw0)
+#define SLEEP_MS 1
 
-static const struct gpio_dt_spec button = GPIO_DT_SPEC_GET(SW0_NODE, gpios);
-
-static struct gpio_callback button_isr_data;
-
-// *dev is a pointer to the GPIO port device that triggered the interrupt
-// pins is a bitmask of the pin that triggered the interrupt on the GPIO port
-void button_isr(const struct device *dev, struct gpio_callback *cb, uint32_t pins) {
-  printk("Button 0 pressed 😊\n");
-}
+// Define some bitmasks to let LEDs represent binary positions
+#define LED_0_COUNT 0b0001
+#define LED_1_COUNT 0b0010  
+#define LED_2_COUNT 0b0100
+#define LED_3_COUNT 0b1000
 
 int main(void) {
-  // used to capture a return value
-  int ret;  
-  if (!gpio_is_ready_dt(&button)) {
+  // Initialize button API
+  if (0 > BTN_init()) {
     return 0;
   }
 
-   // configure the button port as input
-  ret = gpio_pin_configure_dt(&button, GPIO_INPUT);
-  if (0 > ret) {  // Yoda notation
+  // Initialize led API
+  if (0 > LED_init()) {
     return 0;
   }
 
-  // configure button interrupts on a RISING edge (button press)
-  ret = gpio_pin_interrupt_configure_dt(&button, GPIO_INT_EDGE_TO_ACTIVE); 
-  if (0 > ret) {
-    return 0;
-  }
-
-  gpio_init_callback(&button_isr_data, button_isr, BIT(button.pin));
-  gpio_add_callback(button.port, &button_isr_data);
+uint8_t counter = 0b0;
 
   while(1) {
+    if (BTN_check_clear_pressed(BTN0)) {
+      counter++;
 
+      if (counter & LED_0_COUNT) {
+        LED_toggle(LED0);
+      }
+
+      if (15 == counter) {
+        counter = 0;
+      }
+    }
+
+    k_msleep(SLEEP_MS);
   }
   
 	return 0;
